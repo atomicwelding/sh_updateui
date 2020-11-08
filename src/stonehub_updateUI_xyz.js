b8943285512f31d117454f9a8ade75f2986ed326
geode-booster <73887467+geode-booster@users.noreply.github.com>
2020-11-08 23:07:55 +0100

    Bug fixes
    Market buy/sell + inventory/vault and some better error handling

--- src/stonehub_updateUI_xyz.js
+++ src/stonehub_updateUI_xyz.js
@@ -16,7 +16,8 @@ class Stonehub_updateUI_xyz {
         this.xyz_market_HTML = "";
         this.xyz_inventory_items = [];
         this.xyz_market_items = [];
-        this.xyz_refresh_rate = 5000;
+        this.xyz_active_market_tag = "";
+        this.xyz_refresh_rate = 2000;
     }
 
     error_handler(that, e) {
@@ -46,13 +47,18 @@ Stonehub_updateUI_xyz.prototype.xyz_main = function(that) {
 Stonehub_updateUI_xyz.prototype.xyz_get_inventory_HTML = function(that) {
     that.xyz_inventory_HTML = "";
     that.xyz_inventory_items = [];
-    if(document.getElementsByClassName("inventory-container-all-items").length==0) return; // Leave if inventory isn't being displayed
-    that.xyz_inventory_HTML = document.getElementsByClassName("inventory-container-all-items")[0].childNodes[0];
+    if (! document.getElementsByClassName("inventory-panel")[0]) {return;} // Inventory isn't being displayed => Leave
+    that.xyz_inventory_HTML = document.getElementsByClassName("inventory-container-all-items")[0].children[0];
     for (var i = 0; i < that.xyz_inventory_HTML.childElementCount; i++) {
-        let item_node = that.xyz_inventory_HTML.childNodes[12+i];
+        let item_node = that.xyz_inventory_HTML.children[i];
         if(item_node){
             let node_value = item_node.attributes['data-for'].nodeValue;
-            let item_name = node_value.substring(7,node_value.indexOf("stockpile"));
+            let item_name = ""
+            if (node_value.includes("stockpile", 1)) {
+                item_name = node_value.substring(7,node_value.indexOf("stockpile"));
+            } else {
+                item_name = node_value.substring(7,node_value.indexOf("vault"));
+            }
             that.xyz_inventory_items.push([item_name,]);
         }
     }
@@ -61,10 +67,24 @@ Stonehub_updateUI_xyz.prototype.xyz_get_inventory_HTML = function(that) {
 Stonehub_updateUI_xyz.prototype.xyz_get_market_HTML = function(that) {
     that.xyz_market_HTML = "";
     that.xyz_market_items = [];
-    if(document.getElementsByClassName("marketplace-content").length==0) return; // Leave if market isn't being displayed
-    that.xyz_market_HTML = document.getElementsByClassName("marketplace-content")[0].childNodes[0];
+    that.xyz_active_market_tag = ""
+    if (! document.getElementsByClassName("marketplace-content")[0]) {return;} // Market isn't open => Leave
+    if (document.getElementsByClassName("marketplace-sell-items all-items")[0]) {
+        // market is open on "Sell" tab
+        that.xyz_active_market_tag = "marketplace-sell-items all-items";
+    } else {
+        // market is open on "Buy" tab
+        that.xyz_active_market_tag = "marketplace-content";
+    }
+    if (that.xyz_active_market_tag=="marketplace-sell-items all-items") {
+        // Since we're unable to read item names in sell tab, let's just copy the inventory list since it must be the same (tricky but should work), then leave
+        that.xyz_market_HTML = document.getElementsByClassName(that.xyz_active_market_tag)[0];
+        that.xyz_market_items = that.xyz_inventory_items;
+        return;
+    }
+    that.xyz_market_HTML = document.getElementsByClassName(that.xyz_active_market_tag)[0].children[0];
     for (var i = 0; i < that.xyz_market_HTML.childElementCount; i++) {
-        let current_item_node = that.xyz_market_HTML.childNodes[i].childNodes[0].childNodes[0];
+        let current_item_node = that.xyz_market_HTML.children[i].children[0].children[0];
         if(current_item_node){
             let item_name = current_item_node.attributes['alt'].nodeValue;
             that.xyz_market_items.push([item_name,]);
@@ -75,7 +95,7 @@ Stonehub_updateUI_xyz.prototype.xyz_get_market_HTML = function(that) {
 Stonehub_updateUI_xyz.prototype.xyz_update_inventory_HTML = function(that) {
     // Create HTML div into the item node so we can display the price onto it
     for (var i = 0; i < that.xyz_inventory_items.length; i++) {
-        let item_node = that.xyz_inventory_HTML.childNodes[i+12];
+        let item_node = that.xyz_inventory_HTML.children[i];
         if(item_node){
             if(item_node.getElementsByClassName("price").length==0){
                 // If the div was not created yet, create it with adapted CSS style and also move down the enchant icon
@@ -102,7 +122,7 @@ Stonehub_updateUI_xyz.prototype.xyz_update_inventory_HTML = function(that) {
 Stonehub_updateUI_xyz.prototype.xyz_update_market_HTML = function(that) {
     // Create HTML div into the item node so we can display the price onto it
     for (var i = 0; i < that.xyz_market_items.length; i++) {
-        let item_node = that.xyz_market_HTML.childNodes[i].childNodes[0];
+        let item_node = that.xyz_market_HTML.children[i].children[0];
         if(item_node){
             if(item_node.getElementsByClassName("price").length==0){
                 // If the div was not created yet, create it with adapted CSS style and also move down the enchant icon
@@ -115,6 +135,12 @@ Stonehub_updateUI_xyz.prototype.xyz_update_market_HTML = function(that) {
                 newNode.style.fontSize = "9px";
                 var lastNode = item_node.lastElementNode;
                 item_node.insertBefore(newNode, lastNode);
+                if(that.xyz_active_market_tag == "marketplace-sell-items all-items"){
+                    var enchantNode = item_node.getElementsByClassName("item-enchant").item(0);
+                    enchantNode.style.position = "absolute";
+                    enchantNode.style.top = "8px";
+                    enchantNode.style.left = "0px";
+                }
             }
             // Populate the div with xyz API current price
             item_node.getElementsByClassName("price").item(0).textContent = that.xyz_market_items[i][1] ? that.xyz_market_items[i][1].toString() : 'loading...';
