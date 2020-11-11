5e62c2d50844d6f09fd973cf3f440b6e94810e48
atomicwelding <ctfweld@gmail.com>
2020-11-11 18:15:00 +0100

    fix loaded state

--- src/stonehub_updateUI_xyz.js
+++ src/stonehub_updateUI_xyz.js
@@ -43,8 +43,17 @@ class Stonehub_updateUI_xyz {
 
     start() {
         let that = this;
-        that.set_status(that);
-        that.retrieve_status_div(that);
+        
+        // wait for loading to complete, then check which ext is activated
+        let page_ready = setInterval(() =>{
+            if(document.readyState == 'complete'){
+                clearInterval(page_ready);
+                that.set_status(that);
+                that.retrieve_status_div(that)
+            }
+            ;
+        }, 200);
+
         // launch xyz prices (xyz) daemon
         setInterval(() => {
             try {
